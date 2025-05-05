ee23cafefc2ebfcd5d52b1a921aec81d9ad02f27
sebayashi-tomoya <>
2025-05-05 10:37:27 +0900

    公式ドキュメントのサンプルコードお試し

--- DelegateAndLambda/Program.cs
+++ DelegateAndLambda/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+namespace DelegateAndLambda
+{
+    public class Program
+    {
+        public delegate string Reverse(string s);
+
+        static string ReverseString(string s)
+        {
+            return new string(s.Reverse().ToArray());
+        }
+
+        static void Main(string[] args)
+        {
+            Reverse rev = ReverseString;
+
+            Console.WriteLine(rev("a string"));
+        }
+    }
+}
