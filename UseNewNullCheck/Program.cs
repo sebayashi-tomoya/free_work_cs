d3c80f756a041e5b8353f52c75158254ea5ad5ae
sebayashi-tomoya <>
2025-07-21 11:45:47 +0900

    C#14のnullチェック

---
+++ UseNewNullCheck/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection.PortableExecutable;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // まだC#14が使えなかった。
+        Console.WriteLine("Hello World!");
+
+        SetName(null);
+    }
+
+    public static void SetName(Item item)
+    {
+        item?.Name = "hoge";
+    }
+}
+
+public class Item
+{
+    public string Name { get; set; }
+
+    public Item(string name)
+    {
+        this.Name = name;
+    }
+}
