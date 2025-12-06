40aa6b8f7ad95c8cf0ddc699eff7a9a6fcf89be9
sebayashi-tomoya <>
2025-12-06 12:21:07 +0900

    ThreadPoolを使ってみる

---
+++ UseThreadPool/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace UseThreadPool
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine($"Main thread ID: {Thread.CurrentThread.ManagedThreadId}");
+
+            ThreadPool.SetMaxThreads(1, 1);
+
+            ThreadPool.QueueUserWorkItem(Hello);
+            ThreadPool.QueueUserWorkItem(Hello);
+            ThreadPool.QueueUserWorkItem(Hello);
+            ThreadPool.QueueUserWorkItem(Hello);
+            Console.WriteLine($"メインスレッドからこんにちは! ID:{Environment.CurrentManagedThreadId}");
+        }
+
+        private static void Hello(Object stateInfo)
+        {
+            Console.WriteLine($"スレッドプールからこんにちは! ID:{Environment.CurrentManagedThreadId}");
+        }
+    }
+}
